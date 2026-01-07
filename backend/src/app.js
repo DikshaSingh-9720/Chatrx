8dc49b4ddba44f57f077325c688e69975d16141a
Diksha Singh <dikshasingh25202520@gmail.com>
2026-01-07 20:45:32 +0530

    env file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,11 @@
 import express from "express";
 import { createServer } from "node:http";
-
-import { Server } from "socket.io";
-
 import mongoose from "mongoose";
-import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
-const io = connectToSocket(server);
-
 
 app.set("port", (process.env.PORT || 8000))
 app.use(cors());
@@ -23,18 +16,13 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://Diksha:diksha9720@videocall.9mjquwn.mongodb.net/");
+    const connectionDb = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
         console.log("LISTENING ON PORT 8000")
     });
-
-
-
 }
-
-
 
 start();
 
